7d1fb999d0d9dfbb0329eba45c507e9efee1f014
nghiaphamsg <nghiaphamsg@gmail.com>
2021-04-09 16:47:34 +0900

    date 4

---
+++ 007_3D_Cube/include/shaders.h
@@ -0,0 +1,137 @@
+#ifndef __SHADERS_H__
+#define __SHADERS_H__
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class Shaders
+{
+public:
+	unsigned int ID;
+
+    /* Constructor generates the shader on the fly */
+	Shaders(const char* vertexPath, const char* fragmentPath)
+	{
+        /* 1. retrieve the vertex/fragment source code from filePath */
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+
+        /* Ensure ifstream objects can throw exceptions */
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try
+        {
+            /* Open files */
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+
+            /* Read file's buffer contents into streams */
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+
+            /* Close file handlers */
+            vShaderFile.close();
+            fShaderFile.close();
+
+            /* Convert stream into string */
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        }
+        catch (std::ifstream::failure& e)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        }
+
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        /* 2. compile shaders */
+        unsigned int vertex, fragment;
+        /* Vertex shader */
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+
+        /* Fragment Shader */
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+
+        /* Shader Program */
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+
+        /* Delete the shaders as they're linked into our program now and no longer necessary */
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+	}
+
+	/* Activate the shader */
+	void use()
+	{
+		glUseProgram(ID);
+	}
+
+    /* Set uniform */
+    void setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setMat4(const std::string& name, const glm::mat4& mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+	~Shaders()
+	{
+        glDeleteProgram(ID);
+	}
+
+private:
+    void checkCompileErrors(unsigned int shader, std::string type)
+    {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: "
+                          << type << "\n" << infoLog
+                          << "\n ------------------"
+                          << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: "
+                          << type << "\n" << infoLog 
+                          << "\n ------------------" 
+                          << std::endl;
+            }
+        }
+    }
+};
+
+#endif	/* __SHADERS_H__ */
---
+++ 007_3D_Cube/src/main.cpp
@@ -0,0 +1,264 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x2.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <SOIL2.h>
+
+#include <iostream>
+#include <filesystem>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "shaders.h"
+
+void frameBufferResizeCallback(GLFWwindow* window, int fbw, int fbh);
+void processInput(GLFWwindow* window);
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    /* Init GLFW */
+    if (!glfwInit())
+    {
+        std::cout << "GLFW init failed!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    /* Setup GLFW window properties */
+    /* OpenGL version 3.3 */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+    /* Core profile is no backwards compatibility */
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    /* Allow forward compatibility (Mac OS) */
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+    /* Lock resize window */
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    /* Create a new window */
+    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Basic", NULL, NULL);
+    if (mainWindow == nullptr)
+    {
+        std::cout << "GLFW window creation failed!!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Get buffer size information */
+    int buffWidth = 0;
+    int buffHeight = 0;
+    glfwSetFramebufferSizeCallback(mainWindow, frameBufferResizeCallback);
+
+    /* Tell GLFW to make the context of our window the main context on the current thread */
+    glfwMakeContextCurrent(mainWindow);
+
+    /* Allow modern extension features */
+    glewExperimental = GL_TRUE;
+
+    /* Need window & OpenGL context */
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "GLEW init failed!!!" << std::endl;
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Configure global OpenGL state */
+    glEnable(GL_DEPTH_TEST);
+
+    /* Build and compile our shader program
+       Vertex shader */
+    Shaders ourShader("4.5.shader.vs", "4.5.shader.fs");
+
+    /* Set up vertex data (and buffer(s)) and configure vertex attributes */
+    /* 36 vertices (6 faces * 2 triangles * 3 vertices each) */
+    float vertices[] = {
+         /* Store 4 vertices for the rectangle */
+         // position        // texture coords
+         //0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // top right
+         //0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // bottom right
+         //-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,   // bottom left
+         //-0.5f,  0.5f, 0.0f, 0.0f, 1.0f,   // top left
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f
+    };
+
+    unsigned int VBO, VAO, texture;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    /* Bind the Vertex Array Object first, then bind and set vertex buffer(s), 
+       and then configure vertex attributes(s) */
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Set position attribute */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    /* Set texture coord attribute */
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    /* Note that this is allowed, the call to glVertexAttribPointer registered VBO
+       as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    /* Load & Create a texture */
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    /* Load image */
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("C:/Workspace/Project/OpenGL/image/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    /* Render loop */
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        /* Input event */
+        processInput(mainWindow);
+
+        /* Clear window */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        /* Draw */
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        
+
+        // Create transformations
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+        // Retrieve the matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+
+        // Pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        ourShader.setMat4("projection", projection);
+        ourShader.use();
+
+        // Draw rectangle
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        /* Using to render to during this render iteration
+           and show it as output to the screen */
+        glfwSwapBuffers(mainWindow);
+
+        /* Checks if any events are triggered
+           (like keyboard input or mouse movement events)
+           updates the window state */
+        glfwPollEvents();
+    }
+
+    /* De-allocate all resources */
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+void frameBufferResizeCallback(GLFWwindow* window, int fbw, int fbh)
+{
+    glViewport(0, 0, fbw, fbh);
+}
+
+void processInput(GLFWwindow* window)
+{
+    /* Check whether the user has pressed the escape key then close window */
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    /* Press 1 for filled */
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    /* Press 2 for wireframe */
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    /* Press 3 for point */
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+}
