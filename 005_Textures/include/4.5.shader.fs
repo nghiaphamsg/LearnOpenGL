e55d874935b041db14c8b5f393b7db7d4b87e574
nghiaphamsg <nghiaphamsg@gmail.com>
2021-04-09 11:19:47 +0900

    date 4

---
+++ 005_Textures/include/4.5.shader.fs
@@ -0,0 +1,13 @@
+#version 450 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// Texture sampler
+uniform sampler2D texture;
+
+void main()
+{
+    FragColor = texture(texture, TexCoord) * vec4(ourColor, 1.0);
+}
